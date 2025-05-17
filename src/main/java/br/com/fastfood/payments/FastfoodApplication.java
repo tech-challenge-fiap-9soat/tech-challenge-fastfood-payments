b5d85c9dd306342d0644106baf55428d3a2f5439
Samuel Levi <samuellevi.job@gmail.com>
2025-05-17 20:27:31 -0300

    fixing feign client

--- src/main/java/br/com/fastfood/payments/FastfoodApplication.java
+++ src/main/java/br/com/fastfood/payments/FastfoodApplication.java
@@ -2,10 +2,11 @@ package br.com.fastfood.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import javax.annotation.processing.Generated;
 
-@Generated("main")
+@EnableFeignClients(basePackages = "br.com.fastfood.payments.gateways.client")
 @SpringBootApplication
 public class FastfoodApplication {
 
--- src/main/java/br/com/fastfood/payments/gateways/client/PagamentoClient.java
+++ src/main/java/br/com/fastfood/payments/gateways/client/PagamentoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "pagamento-client", url = "${external.api.endpoint}")
+@FeignClient(name = "pagamento-client", url = "${external.api.url}")
 public interface PagamentoClient {
 
     @PostMapping
