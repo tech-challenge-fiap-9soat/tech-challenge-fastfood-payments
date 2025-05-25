31d1755ef54a00f31203a77a8cc2ac27b7c7b14b
Samuel Levi <samuellevi.job@gmail.com>
2025-05-25 18:59:18 -0300

    removing symbol

--- src/main/java/br/com/fastfood/payments/gateways/client/PedidoClient.java
+++ src/main/java/br/com/fastfood/payments/gateways/client/PedidoClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "pedido-client", url = "${external.order.url}")
 public interface PedidoClient {
 
-    @PatchMapping(value = "/pedido/proxima-operacao/${idPedido}")
+    @PatchMapping(value = "/pedido/proxima-operacao/{idPedido}")
     void updateStatusPedido(@PathVariable Long idPedido);
 
 }
